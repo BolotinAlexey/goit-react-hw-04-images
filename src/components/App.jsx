af5140c525d98a2472813c38e577fd6b168316a9
Oleksii Bolotin <samkiral73@gmail.com>
2023-03-27 14:38:24 +0200

    init + some did App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { Wrap } from 'components/App.styled.js';
 
 import { PER_PAGE } from 'services/constants';
@@ -9,107 +9,102 @@ import Button from 'components/Button/Button';
 import Loader from 'components/Loader/Loader';
 import Modal from 'components/Modal/Modal';
 
-class App extends Component {
-  state = {
-    // resesarch word
-    word: '',
+function App() {
+  // state = {
+  //   // resesarch word
+  //   word: '',
 
-    // array of gallery objects
-    gallery: [],
+  //   // array of gallery objects
+  //   gallery: [],
 
-    //flag shower of spiner
-    isLoading: false,
+  //   //flag shower of spiner
+  //   isLoading: false,
 
-    //flag shower button of 'read-more'
-    isMore: false,
+  //   //flag shower button of 'read-more'
+  //   isMore: false,
 
-    // {img:refer to large img,alt:text of attribute alt}
-    modalImg: null,
-  };
+  //   // {img:refer to large img,alt:text of attribute alt}
+  //   modalImg: null,
+  // };
 
-  componentDidUpdate(_, { gallery }) {
-    if (this.state.gallery.length === 0) window.scrollTo({ top: 0 });
-    if (gallery.length !== this.state.gallery.length)
-      window.scrollTo({
-        top: document.querySelector('body').scrollHeight,
-        behavior: 'smooth',
-      });
-  }
+  const [word, setWord] = useState(''); // resesarch word
+  const [gallery, setGallery] = useState([]); // array of gallery objects
+  const [isLoading, setisLoading] = useState(false); //flag shower of spiner
+  const [isMore, setIsMore] = useState(false); //flag shower button of 'read-more'
+  const [modalImg, setModalImg] = useState(null); // {img:refer to large img,alt:text of attribute alt}
+
+  // componentDidUpdate(_, { gallery }) {
+  //   if (this.state.gallery.length === 0) window.scrollTo({ top: 0 });
+  //   if (gallery.length !== this.state.gallery.length)
+  //     window.scrollTo({
+  //       top: document.querySelector('body').scrollHeight,
+  //       behavior: 'smooth',
+  //     });
+  // }
 
   // helper function for working with request api
-  requestToApi = async (word, currentGallery) => {
-    this.setState({ isLoading: true });
+  const requestToApi = async (word, currentGallery) => {
+    setisLoading(true);
     try {
       const { reqGallery, isMore } = await API.readData(
         word,
         Math.floor(currentGallery.length / PER_PAGE) + 1
       );
 
-      this.setState({
-        gallery: [...currentGallery, ...reqGallery],
-        isMore,
-        word,
-      });
+      setGallery([...currentGallery, ...reqGallery]);
+      setIsMore(isMore);
+      setWord(word);
     } catch (error) {
       console.error(error);
-      this.setState({
-        word: '',
-        gallery: [],
-        isMore: false,
-      });
+      setGallery([]);
+      setIsMore(false);
+      setWord('');
     } finally {
-      this.setState({ isLoading: false });
+      setisLoading(false);
     }
   };
 
   // submit new word
-  handlerSubmit = word => {
-    this.setState({
-      word,
-      gallery: [],
-      isLoading: true,
-    });
+  const handlerSubmit = word => {
+    setWord(word);
+    setGallery([]);
+    setisLoading(true);
 
-    this.requestToApi(word, []);
+    requestToApi(word, []);
   };
 
   // press button 'read more'
-  handlerMore = () => {
-    this.requestToApi(this.state.word, this.state.gallery);
+  const handlerMore = () => {
+    requestToApi(word, gallery);
   };
 
   // click to element of gallery
-  onClickToGallery = modalImg => {
-    this.setState({ modalImg });
+  const onClickToGallery = modalImg => {
+    setModalImg(modalImg);
   };
 
   // close modal window
-  closeModal = () => this.setState({ modalImg: null });
+  const closeModal = () => setModalImg(null);
 
-  render() {
-    const { gallery, isMore, isLoading, modalImg } = this.state;
-    return (
-      <Wrap>
-        {/* modal */}
-        {modalImg && (
-          <Modal closeWindow={this.closeModal}>
-            <img src={modalImg?.img} alt={modalImg?.alt} />
-          </Modal>
-        )}
+  return (
+    <Wrap>
+      {/* modal */}
+      {modalImg && (
+        <Modal closeWindow={closeModal}>
+          <img src={modalImg?.img} alt={modalImg?.alt} />
+        </Modal>
+      )}
 
-        {/* search bar */}
-        <Searchbar onSubmit={this.handlerSubmit} isDisabled={isLoading} />
-        {/* gallery list */}
-        <ImageGallery
-          gallery={gallery}
-          onClickToGallery={this.onClickToGallery}
-        />
-        {/* loader */}
-        <Loader visible={isLoading} />
-        {/* button 'Load more' */}
-        {isMore && <Button isDisabled={isLoading} onClick={this.handlerMore} />}
-      </Wrap>
-    );
-  }
+      {/* search bar */}
+      <Searchbar onSubmit={handlerSubmit} isDisabled={isLoading} />
+      {/* gallery list */}
+      <ImageGallery gallery={gallery} onClickToGallery={onClickToGallery} />
+      {/* loader */}
+      <Loader visible={isLoading} />
+      {/* button 'Load more' */}
+      {isMore && <Button isDisabled={isLoading} onClick={handlerMore} />}
+    </Wrap>
+  );
 }
+
 export default App;
